2876e1f5cb5ce30043b4e2d375f18fbc8e2e0b15
Thaise Medeiros <thai@Thaises-MacBook-Air.local>
2023-06-09 17:39:50 -0300

    Instrumentation to Azure App Insights

--- src/MemoryLeak/MemoryLeak/Startup.cs
+++ src/MemoryLeak/MemoryLeak/Startup.cs
@@ -1,3 +1,5 @@
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.ApplicationInsights.Extensibility.EventCounterCollector;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,6 +21,28 @@ namespace MemoryLeak
         {
             services.AddRazorPages();
             services.AddControllers();
+
+            var appInsigths = Configuration.GetConnectionString("app");
+
+            var applicationInsightsServiceOptions = new ApplicationInsightsServiceOptions();
+            applicationInsightsServiceOptions.EnableEventCounterCollectionModule = true;
+            applicationInsightsServiceOptions.ConnectionString = appInsigths;
+            services.AddApplicationInsightsTelemetry(applicationInsightsServiceOptions);
+
+            services.ConfigureTelemetryModule<EventCounterCollectionModule>((module, o) => {
+                module.Counters.Add(new EventCounterCollectionRequest("System.Runtime", "time-in-gc"));
+                module.Counters.Add(new EventCounterCollectionRequest("System.Runtime", "cpu-usage"));
+                module.Counters.Add(new EventCounterCollectionRequest("System.Runtime", "gen-0-size"));
+                module.Counters.Add(new EventCounterCollectionRequest("System.Runtime", "gen-1-size"));
+                module.Counters.Add(new EventCounterCollectionRequest("System.Runtime", "gen-2-size"));
+                module.Counters.Add(new EventCounterCollectionRequest("System.Runtime", "gc-heap-size"));
+                module.Counters.Add(new EventCounterCollectionRequest("System.Runtime", "gen-0-gc-count"));
+                module.Counters.Add(new EventCounterCollectionRequest("System.Runtime", "gen-1-gc-count"));
+                module.Counters.Add(new EventCounterCollectionRequest("System.Runtime", "gen-2-gc-count"));
+                module.Counters.Add(new EventCounterCollectionRequest("System.Runtime", "loh-size"));
+                module.Counters.Add(new EventCounterCollectionRequest("System.Runtime", "gc-fragmentation"));
+                module.Counters.Add(new EventCounterCollectionRequest("System.Runtime", "gc-fragmentation"));
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
